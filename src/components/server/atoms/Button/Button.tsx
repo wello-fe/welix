3337df2c96a56d839a40ef14b96c21b07cdb65bb
Chung-il Jung <chungil@wello.info>
2024-04-15 19:56:33 +0900

    feat: tailwind eslint plugin (#8)

--- src/components/server/atoms/Button/Button.tsx
+++ src/components/server/atoms/Button/Button.tsx
@@ -10,29 +10,29 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/class';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center border rounded-lg text-sm font-medium transition ease-in-out duration-150 focus:outline-none focus:ring-2',
+  'inline-flex items-center justify-center rounded-lg text-sm font-medium transition duration-150 ease-in-out focus:outline-none focus:ring-2',
   {
     variants: {
       theme: {
         'primary-fill':
-          'bg-deepblue-500 text-white hover:bg-deepblue-700 focus:ring-deepblue-500/50 font-pretendard text-lg font-bold leading-6 text-left',
+          'bg-deepblue-500 text-left text-lg font-bold leading-6 text-white hover:bg-deepblue-700 focus:ring-deepblue-500/50',
         'primary-line':
-          'border border-deepblue-500 text-deepblue-500 bg-transparent hover:bg-deepblue-50 focus:ring-deepblue-500/50 font-pretendard text-lg font-bold leading-6 text-left',
+          'border border-deepblue-500 bg-transparent text-left text-lg font-bold leading-6 text-deepblue-500 hover:bg-deepblue-50 focus:ring-deepblue-500/50',
         'primary-text':
-          'text-deepblue-500 bg-transparent hover:bg-deepblue-50 font-pretendard text-lg font-bold leading-6 text-left',
+          'bg-transparent text-left text-lg font-bold leading-6 text-deepblue-500 hover:bg-deepblue-50',
         'sub-fill':
-          'bg-grayscale-300 text-grayscale-800 hover:bg-grayscale-300 focus:ring-grayscale-300/50 font-pretendard text-lg font-bold leading-6 text-left',
+          'bg-grayscale-300 text-left text-lg font-bold leading-6 text-grayscale-800 hover:bg-grayscale-300 focus:ring-grayscale-300/50',
         'sub-line':
-          'border border-grayscale-300 text-grayscale-900 bg-transparent hover:bg-grayscale-300 focus:ring-grayscale-300/50 font-pretendard text-lg font-bold leading-6 text-left',
+          'border border-grayscale-300 bg-transparent text-left text-lg font-bold leading-6 text-grayscale-900 hover:bg-grayscale-300 focus:ring-grayscale-300/50',
         'sub-text':
-          'text-grayscale-800 bg-transparent hover:bg-grayscale-200 font-pretendard text-lg font-bold leading-6 text-left',
+          'bg-transparent text-left text-lg font-bold leading-6 text-grayscale-800 hover:bg-grayscale-200',
       },
       height: {
-        H56: 'h-14 py-4 px-6',
-        H48: 'h-12 py-3 px-6',
-        H40: 'h-10 py-3 px-4',
-        H32: 'h-8 py-2 px-4',
-        H26: 'h-6.5 py-1 px-2.5',
+        H56: 'h-14 px-6 py-4',
+        H48: 'h-12 px-6 py-3',
+        H40: 'h-10 px-4 py-3',
+        H32: 'h-8 px-4 py-2',
+        H26: 'h-6 px-2.5 py-1',
       },
       roundness: {
         rectangle: 'rounded-lg',
