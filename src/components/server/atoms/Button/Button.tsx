b87884d518c54dd0c16b3f28fec22d3ac1a02857
정충일 <chungil@wello.info>
2024-04-15 21:12:51 +0900

    feat: button disabled

--- src/components/server/atoms/Button/Button.tsx
+++ src/components/server/atoms/Button/Button.tsx
@@ -15,24 +15,24 @@ const buttonVariants = cva(
     variants: {
       theme: {
         'primary-fill':
-          'bg-deepblue-500 text-left text-lg font-bold leading-6 text-white hover:bg-deepblue-700 focus:ring-deepblue-500/50',
+          'bg-deepblue-500 text-left text-lg font-bold leading-6 text-white hover:bg-deepblue-700 focus:ring-deepblue-500/50 disabled:hover:bg-deepblue-500',
         'primary-line':
-          'border border-deepblue-500 bg-transparent text-left text-lg font-bold leading-6 text-deepblue-500 hover:bg-deepblue-50 focus:ring-deepblue-500/50',
+          'border border-deepblue-500 bg-transparent text-left text-lg font-bold leading-6 text-deepblue-500 hover:bg-deepblue-50 focus:ring-deepblue-500/50 disabled:border-deepblue-500 disabled:text-deepblue-500 disabled:hover:bg-transparent',
         'primary-text':
-          'bg-transparent text-left text-lg font-bold leading-6 text-deepblue-500 hover:bg-deepblue-50',
+          'bg-transparent text-left text-lg font-bold leading-6 text-deepblue-500 hover:bg-deepblue-50 disabled:text-deepblue-500 disabled:hover:bg-transparent',
         'sub-fill':
-          'bg-grayscale-300 text-left text-lg font-bold leading-6 text-grayscale-800 hover:bg-grayscale-300 focus:ring-grayscale-300/50',
+          'bg-grayscale-300 text-left text-lg font-bold leading-6 text-grayscale-800 hover:bg-grayscale-300 focus:ring-grayscale-300/50 disabled:hover:bg-grayscale-300',
         'sub-line':
-          'border border-grayscale-300 bg-transparent text-left text-lg font-bold leading-6 text-grayscale-900 hover:bg-grayscale-300 focus:ring-grayscale-300/50',
+          'border border-grayscale-300 bg-transparent text-left text-lg font-bold leading-6 text-grayscale-900 hover:bg-grayscale-300 focus:ring-grayscale-300/50 disabled:border-grayscale-300 disabled:text-grayscale-900 disabled:hover:bg-transparent',
         'sub-text':
-          'bg-transparent text-left text-lg font-bold leading-6 text-grayscale-800 hover:bg-grayscale-200',
+          'bg-transparent text-left text-lg font-bold leading-6 text-grayscale-800 hover:bg-grayscale-200 disabled:text-grayscale-800 disabled:hover:bg-transparent',
       },
       height: {
-        H56: 'h-14 px-6 py-4',
-        H48: 'h-12 px-6 py-3',
-        H40: 'h-10 px-4 py-3',
-        H32: 'h-8 px-4 py-2',
-        H26: 'h-6 px-2.5 py-1',
+        H56: 'h-14 gap-1.5 px-6 py-4',
+        H48: 'h-12 gap-1.5 px-6 py-3',
+        H40: 'h-10 gap-1 px-4 py-3',
+        H32: 'h-8 gap-1 px-4 py-2',
+        H26: 'h-6 gap-1 px-2.5 py-1',
       },
       roundness: {
         rectangle: 'rounded-lg',
@@ -48,7 +48,7 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends RadixButtonProps,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
   color?: RadixButtonProps['color'];
@@ -71,7 +71,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Comp
         type={type}
-        className={cn(buttonVariants({ theme, height, roundness, className }))}
+        className={cn(buttonVariants({ theme, height, roundness, className }), {
+          'opacity-50 cursor-not-allowed': props.disabled,
+        })}
         ref={ref}
         {...props}
       />
